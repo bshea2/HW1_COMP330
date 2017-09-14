a92fba5578f364c29a66e9117aa8ad8e78a655a4
Brendan <bshea2@luc.edu>
2017-09-14 13:55:36 -0500

    Third comit. I changed the scanner to buffered reader after looking at some examples. I was able to parse the text file and give an overall count of each occurance of a word.

--- src/noteReader.java
+++ src/noteReader.java
@@ -1,5 +1,8 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -14,31 +17,38 @@ public class noteReader {
 		//find the file
 		File noteFile = new File(fileLocation);
 		
-			//use scanner that will read file
-			Scanner noteScanner = new Scanner(noteFile);
+		//use bufferedreader that will read file
 			
-			String nextLine = noteScanner.nextLine();
+		BufferedReader reader = new BufferedReader(new FileReader(noteFile));
 			
-			//System.out.println("Next line: " + nextLine);
+		String line = reader.readLine();
 			
-			Map<String, Integer> frequency = new HashMap<>();
+		Map<String, Integer> frequency = new HashMap<>();
 			
-			while(!nextLine.isEmpty()) {
-				System.out.println("test2");
-				if (!nextLine.trim().equals("")) {
-					String[] words = nextLine.split(" ");
+			while(line != null){
+				//test line to see if the reader is working
+				//System.out.println("processed line" + line);
 				
+				if (!line.trim().equals("")) {
+					//splitting words by spaces between them
+					String[] words = line.split(" ");
+				
+					//iterating through each word in the array of words
 					for (String word : words) {
+						
 						//in case the word is null or an empty space we just want to continue
 						if (word == null || word.trim().equals("")) {
 							continue;
 						}
+						
 						//to ensure that capital letters don't impact the counting
 						String processed = word.toLowerCase();
+						
 						//if we already have placed this word in the map, we increment its value by 1
 						if (frequency.containsKey(processed)) {
 							frequency.put(processed, frequency.get(processed) +1);
 						}
+						
 						//we add this new word to the map and make its incremental value = 1
 						else {
 							frequency.put(processed, 1);
@@ -46,10 +56,10 @@ public class noteReader {
 						
 					}
 				}
-				
+				line = reader.readLine();
 			}
-		//	System.out.println("test");
-		//System.out.println(frequency);
+			
+		System.out.println(frequency);
 		
 	}
 }
